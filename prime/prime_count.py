a9d80f6c0c7457bc21cf8a960ee13121cd075783
MAMIDI RAVI PRAVEEN <121561557+RAVI-PRAVEEN@users.noreply.github.com>
2024-10-17 19:50:19 +0530

    Create prime_count.py

---
+++ prime/prime_count.py
@@ -0,0 +1,16 @@
+import math
+
+def isprime(n):
+    if n <= 1:
+        return False
+    elif n == 2:  # 2 is the smallest prime number
+        return True
+    elif n % 2 == 0:  # If n is even and greater than 2, it's not prime
+        return False
+    # Check divisors from 3 up to sqrt(n)
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+n=int(input())
+print(isprime(n))
